2d05d1cc35dbd27758cf5276abf0fbc68d727e85
pxg950110@163.com <pxg950110@163.com>
2020-04-23 10:36:13 +0800

    更新内容

--- ch-pxg-cloud-author-server/src/main/java/ch/pxg/cloud/chpxgcloudauthorserver/config/WebSecurityConfigurer.java
+++ ch-pxg-cloud-author-server/src/main/java/ch/pxg/cloud/chpxgcloudauthorserver/config/WebSecurityConfigurer.java
@@ -8,8 +8,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
---
+++ ch-pxg-cloud-author-server/src/main/java/ch/pxg/cloud/chpxgcloudauthorserver/controller/AuthorController.java
@@ -0,0 +1,24 @@
+package ch.pxg.cloud.chpxgcloudauthorserver.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 2020/4/23  10:35
+ * </p>
+ * <p>
+ * @author pxg
+ * @emil pxg950110@163.com
+ * @Date 2020/4/23
+ * @Version 1.0.0
+ * @description </p>
+ */
+@RestController
+@RequestMapping("author")
+@Api("用户权限相关操作接口")
+public class AuthorController {
+
+
+}
